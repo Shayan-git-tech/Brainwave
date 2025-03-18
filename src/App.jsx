5751f588a0d460011bd917b830e7b71ff9e77216
Shayan-git-tech <shayankhan12340@gmail.com>
2025-03-18 21:50:12 +0500

    Deploy React app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+import Header from './components/Header'
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Hero from './components/Hero';
+
+
+function App() {
+ 
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <ButtonGradient />
+      
+    </>
+  );
+}
+
+export default App
